c2d69fda819948e1d1f7214eaed184c43087ea6d
Josue Alejandro Moreno Herdoiza <jamoreno238@hotmail.com>
2025-01-27 23:13:16 -0500

    Update App.tsx

--- about-me/src/App.tsx
+++ about-me/src/App.tsx
@@ -1,18 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Inicio from "./components/Inicio";
-import 'font-awesome/css/font-awesome.min.css';
-import Contact from "./components/Contact";
-
-
-
+import "font-awesome/css/font-awesome.min.css";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Inicio />} />
-        
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/inicio" element={<Inicio />} />
       </Routes>
     </Router>
   );
